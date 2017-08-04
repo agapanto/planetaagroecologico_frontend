684bc79bb80b7104e426fe4f63a8113e5e05aa28
mcueto <cueto@live.cl>
2017-08-04 15:56:12 -0400

    Add admin route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Admin from '@/components/Admin'
 import Hello from '@/components/Hello'
 
 Vue.use(Router)
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin
     }
   ]
 })
