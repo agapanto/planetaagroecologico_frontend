6807f06726954d27cfb95e9ffb897b3932301fd1
mcueto <cueto@live.cl>
2017-08-07 16:50:09 -0400

    Add right floated dropdown in admin component

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="ui container">
-    Admin
+    <div class="ui right floated teal buttons">
+      <div class="ui button">Nuevo productor</div>
+      <div class="ui floating dropdown icon button">
+        <i class="dropdown icon"></i>
+        <div class="menu">
+          <div class="item"><i class="cloud upload icon"></i> Importar desde csv</div>
+          <!-- <div class="item"><i class="delete icon"></i> Eliminar Productores</div>
+          <div class="item"><i class="hide icon"></i> Deshabilitar Productores</div> -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
