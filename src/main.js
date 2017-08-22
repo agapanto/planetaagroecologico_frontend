960bf85f4d69d839214fdc510af727fd59740a6d
mcueto <cueto@live.cl>
2017-08-22 14:59:44 -0300

    Add auth0 and lockjs

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Auth0Lock from 'auth0-lock'
 
 Vue.config.productionTip = false
 
@@ -13,3 +14,8 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
+// Instantiate a Lock
+var CLIENT_ID = ''
+var CLIENT_DOMAIN = ''
+export var lock = new Auth0Lock(CLIENT_ID, CLIENT_DOMAIN)
