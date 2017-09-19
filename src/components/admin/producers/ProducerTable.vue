6e72eabbd8bdc528385b2e70707e485e20cb57d2
mcueto <cueto@live.cl>
2017-09-19 17:51:19 -0300

    Add ProducersTable and ProducersTableRow components

---
+++ src/components/admin/producers/ProducerTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <table class="ui sortable celled table">
+    <thead>
+      <tr>
+        <th class="sorted ascending">Nombre</th>
+        <th class="">Descripción</th>
+        <th class="">Estado</th>
+      </tr>
+    </thead>
+    <tbody>
+      <producer-table-row v-bind:producer="producer" v-for="producer in producers"></producer-table-row>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import ProducerTableRow from './ProducerTableRow'
+
+export default {
+  name: 'admin-producers-table',
+  props: {
+    producers: {
+      default: function () { return [] }
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  components: {
+    'producer-table-row': ProducerTableRow
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/admin/producers/ProducerTableRow.vue
@@ -0,0 +1,28 @@
+<template>
+  <tr>
+    <td>{{producer.name}}</td>
+    <td>{{producer.description}}</td>
+    <td>{{producer.status}}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'admin-producers-table',
+  props: {
+    producer: {
+      default: function () { return {} }
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
