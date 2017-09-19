4aec331c348b275e131533000ddde4bd501b7849
mcueto <cueto@live.cl>
2017-09-19 17:52:38 -0300

    Add Producers admin view and router

---
+++ src/components/admin/producers/Index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="ui container">
+
+    <admin-menu></admin-menu>
+
+    <producer-table v-bind:producers="producers"></producer-table>
+
+  </div>
+</template>
+
+<script>
+import AdminMenu from '../../admin/AdminMenu'
+import ProducerTable from './ProducerTable'
+// import store from '../../../store'
+
+export default {
+  name: 'admin_importer',
+  data () {
+    return {
+      import_data: []
+    }
+  },
+  methods: {
+  },
+  computed: {
+    producers: function () {
+      return [
+        {
+          'name': 'asd',
+          'description': 'asd',
+          'status': 'active'
+        }
+      ]
+    }
+  },
+  components: {
+    'admin-menu': AdminMenu,
+    'producer-table': ProducerTable
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Admin from '@/components/Admin'
 import AdminImporter from '@/components/admin/importer/Index'
+import Producers from '@/components/admin/producers/Index'
 import Hello from '@/components/Hello'
 import Login from '@/components/Login'
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/admin',
       name: 'Admin',
       component: Admin
+    },
+    {
+      path: '/admin/producers',
+      name: 'Producers',
+      component: Producers
     },
     {
       path: '/admin/importer',
